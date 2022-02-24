0e8feedd817b7ab87c4b1624dd20185594140cd8
Shrinivasraddi <97089356+Shrinivasraddi@users.noreply.github.com>
2022-02-24 10:17:31 +0000

    p2

--- p2final.c
+++ p2final.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter the  values of x1,x2,x3,y1,y2,y3\n");
+  scanf(" %f %f %f %f %f %f",x1,x2,x3,y1,y2,y3);
+}
 
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+ float area;
+  area=0.5*(x1*(y2-y3)+ x2*(y3-y1) + x3*(y1-y2));
+  return area;
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, float istriangle)
+{
+if(istriangle==0)
+  printf("the given points %.1f %.1f  %.1f  %.1f  %.1f  %.1f  does not form a triangle\n",x1,x2,x3,y1,y2,y3);
+else
+    printf("the given points %.1f %.1f  %.1f  %.1f  %.1f  %.1f  forms a triangle\n",x1,x2,x3,y1,y2,y3);
+  }
+int main()
+{
+  float a,b,c,x,y,z,result;
+  input_triangle(&a,&b,&c,&x,&y,&z);
+  result = is_triangle(a,b,c,x,y,z);
+  output(a,b,c,x,y,z,result);
+  return 0;
+}
